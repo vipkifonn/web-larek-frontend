9d5265f7fde3b35edc9abc20ce4900b15734d462
Aleksey Nikiforov <Vipkifonn@yandex.ru>
2024-04-18 22:13:25 +0300

    первое ревью

---
+++ src/types/index.ts
@@ -0,0 +1,54 @@
+//тип для продукта
+export type ProductCategory =
+	| 'софт-скил'
+	| 'другое'
+	| 'дополнительное'
+	| 'кнопка'
+	| 'хард-скил';
+
+//тип для оплаты
+export type PaymentMethod = 'онлайн' | 'при получении';
+
+//интерфейс для товаров
+export interface IProduct {
+	id: string;
+	category: ProductCategory;
+	title: string;
+	price: number;
+	description: string;
+	image: string;
+}
+//интерфейс для списка товаров
+export interface IProductList {
+	item: IProduct[];
+}
+//интерфейс для корзины
+export interface IBasket {
+	item: IProduct[];
+	price: number;
+}
+//данные пользователя
+export interface IDeliveryForm {
+	payment?: PaymentMethod;
+	address?: string;
+	email?: string;
+	phone?: string;
+}
+
+//информация в корзине
+export interface IOrder extends IDeliveryForm {
+	total: number;
+	items: string[];
+}
+
+//итоговый счет
+export interface IOrderTotal {
+	id: string;
+	total: number;
+}
+
+//показ карточки товара
+export interface ICard extends IProduct {
+	index?: string;
+	buttonTitle?: string;
+}
